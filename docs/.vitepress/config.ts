d1114957bae1143552fcb5c238f9fb7a9c5345d0
yosipy <todacchi.desu@gmail.com>
2025-11-08 21:37:01 +0900

    move to examples/

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,15 +8,15 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
+      { text: "Examples", link: "/examples/markdown-examples" },
     ],
 
     sidebar: [
       {
         text: "Examples",
         items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
+          { text: "Markdown Examples", link: "/examples/markdown-examples" },
+          { text: "Runtime API Examples", link: "/examples/api-examples" },
         ],
       },
     ],
