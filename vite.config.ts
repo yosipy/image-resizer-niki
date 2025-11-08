ebb30eb47374d5f7b6831ead66d39099caeaad02
yosipy <todacchi.desu@gmail.com>
2025-11-09 04:46:09 +0900

    include wasm file to build resutl

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite"
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    plugins: [wasm(), topLevelAwait()],
     resolve: {
       alias: [{ find: "@lib", replacement: "/lib" }],
     },
